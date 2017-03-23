0cb1ab1f8afc98ab4be757c7e0e07f7ff3d2dbb0
uwaces <uwaces@gmail.com>
2017-03-23 16:32:44 -0400

    Starting API

---
+++ api.py
@@ -0,0 +1,24 @@
+def add(value): # add a node with the value, value iff cur node in null (if you break the BST properties it's on you)
+        pass
+def std_remove(): 
+        pass    # preforms a standard BST remove and remains on the last node traversed too (NULL?)
+def read_closure():
+        pass                  # returns the closure for the node you are currently on
+def write_closure(cl):
+        pass               # sets the closure for the current node 
+def read_gclosure():
+        pass                 # reads the global closure
+def write_gclosure(cl):
+        pass              # writes the closure
+def is_null():
+        pass                       # is the current node null?
+def read_value():
+        pass                    # returns the value of the current node
+def write_value(val):
+        pass                # sets the value of cur node
+def move_right():
+        pass                    # sets cur node to right child
+def move_left():
+        pass                     # ---------------- left ----- 
+def move_parent():
+        pass                   # ------------------ parent 
