e27949e4674c8b23e0c48482f7f354685870f34b
uwaces <uwaces@gmail.com>
2017-03-23 19:09:46 -0400

    Continued work on api. Trying to figure out how to pass refrences by refrence. In order for taversal to parent Nodes may need parent pointer

--- api.py
+++ api.py
@@ -1,7 +1,20 @@
+import BSTDataModel
+
+bst = BSTDataModel.BSTDataModel() 
+
 def add(value): # add a node with the value, value iff cur node in null (if you break the BST properties it's on you)
-        pass
-def std_remove(): 
-        pass    # preforms a standard BST remove and remains on the last node traversed too (NULL?)
+        if bst.current is None:
+                bst.current = BSTDataModel.Node(value)
+def std_remove(): # preforms a standard BST remove and remains on the last node traversed too (NULL?)
+        if bst.current is None:
+                return False  # empty node cannot be removed
+        elif bst.current.l is not None and bst.current.r is not None:
+                bst.current = None # Directly remove node
+                return True
+        elif 
+        successor = bst.current.r
+        while successor.l is not None:
+                
 def read_closure():
         pass                  # returns the closure for the node you are currently on
 def write_closure(cl):
