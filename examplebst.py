749af2f797d4c2cdba479552b326450cb7524c48
uwaces <uwaces@gmail.com>
2017-03-25 09:24:05 -0400

    my goal is to make examplebst to be the basic BST.\n ExampleBST is the implementation of AbstractBST

--- examplebst.py
+++ examplebst.py
@@ -1,13 +1,26 @@
 import AbstractBST
-
+import API
 
 class ExampleBST(AbstractBST.AbstractBST):
         # ExampleBST - example of concrete implementation of AbstractBST
         def __init__(self):
                 super(ExampleBST, self).__init__()
+                self.__api = API()
 
         def insert(self, value):
-                print("Inserted " + str(value))
+                # Assume we start on the root
+                while not self.__api.is_null():
+                        node_value = self.__api.read_value()
+                        if value < node_value:
+                                self.__api.move_left()
+                        elif value > node_value:
+                                self.__api.move_right()
+                        elif value = node_value:
+                                self.__api.inc_count()
+                                break
+                if self.__api.is_null():
+                        self.__api.add(value)
+                        
 
         def delete(self, value):
                 print("Deleted " + str(value))
@@ -16,3 +29,4 @@ class ExampleBST(AbstractBST.AbstractBST):
                 print("Found " + str(value))
 
 x = ExampleBST()
+
