a996e289256b1b9e08336254934fd3d92bcd2f48
uwaces <uwaces@gmail.com>
2017-03-25 09:36:05 -0400

    Finished Untested examplebst

--- examplebst.py
+++ examplebst.py
@@ -7,7 +7,7 @@ class ExampleBST(AbstractBST.AbstractBST):
                 super(ExampleBST, self).__init__()
                 self.__api = API()
 
-        def insert(self, value):
+        def insert(self, value): # WEIRD!! API lets us do insert non-recursivly
                 # Assume we start on the root
                 while not self.__api.is_null():
                         node_value = self.__api.read_value()
@@ -15,7 +15,7 @@ class ExampleBST(AbstractBST.AbstractBST):
                                 self.__api.move_left()
                         elif value > node_value:
                                 self.__api.move_right()
-                        elif value = node_value:
+                        elif value == node_value:
                                 self.__api.inc_count()
                                 break
                 if self.__api.is_null():
@@ -23,10 +23,28 @@ class ExampleBST(AbstractBST.AbstractBST):
                         
 
         def delete(self, value):
-                print("Deleted " + str(value))
+                if self.__api.is_null():
+                        return False
+                elif value == self.__api.read_value():
+                        self.__api.std_remove(value)
+                        return True
+                elif value < self.__api.read_value():
+                        self.__api.move_left()
+                elif value > self.__api.read_value():
+                        self.move_right()
+                return delete(self, value)
 
+                
         def search(self, value):
-                print("Found " + str(value))
+                if self.__api.is_null():
+                        return False
+                elif value == self.__api.read_value():
+                        return True
+                elif value < self.__api.read_value():
+                        self.__api.move_left()
+                elif value > self.__api.read_value():
+                        self.move_right()
+                return search(self, value)
 
 x = ExampleBST()
 
